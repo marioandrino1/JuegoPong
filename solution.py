6d437031f4cb159959ef30d356b1076657c377ba
marioandrino1 <marioandrinogonzalez@gmail.com>
2022-02-23 17:25:50 +0100

    primer trozo

---
+++ solution.py
@@ -0,0 +1,48 @@
+import pygame
+pygame.init()
+
+
+WIDTH,HEIGHT = 700,500
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Pongo")
+
+FPS = 60
+
+
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+
+
+class Paddle:
+    def __init__(self,x,y,width,height):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+
+
+
+def draw(win):
+    win.fill(WHITE)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+                break
+
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
+
