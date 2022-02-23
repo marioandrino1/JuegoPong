75dc5c0505a68dcda36ed75b292d10e9a3505968
marioandrino1 <marioandrinogonzalez@gmail.com>
2022-02-23 20:06:51 +0100

    barras creadas

--- solution.py
+++ solution.py
@@ -11,30 +11,43 @@ FPS = 60
 
 WHITE=(255,255,255)
 BLACK=(0,0,0)
+PADDLE_HEIGHT,PADDLE_WIDHT=100,20
+
 
 
 class Paddle:
+
+    COLOR= WHITE
+
     def __init__(self,x,y,width,height):
         self.x=x
         self.y=y
         self.width=width
         self.height=height
+    def draw(self,win):
+        pygame.draw.rect(win,self.COLOR,(self.x,self.y,self.width,self.height))
 
 
 
-def draw(win):
-    win.fill(WHITE)
+
+
+def draw(win,paddles):
+    win.fill(BLACK)
+
+    for a in paddles:
+        a.draw(win)
     pygame.display.update()
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
-
+    left_paddle=Paddle(10,HEIGHT//2-PADDLE_HEIGHT//2,PADDLE_WIDHT,PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH-10-PADDLE_WIDHT, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDHT, PADDLE_HEIGHT)
 
     while run:
         clock.tick(FPS)
-        draw(WIN)
+        draw(WIN,[left_paddle,right_paddle])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
